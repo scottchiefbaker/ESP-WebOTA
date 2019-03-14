d61834dda9c219ac0418826b57881e8690ad4da8
Scott Baker <scott@perturb.org>
2019-03-13 21:29:53 -0700

    Add an example

---
+++ examples/BlinkOTA/BlinkOTA.ino
@@ -0,0 +1,31 @@
+const char* host     = "ESP32-OTA"; // Used for MDNS resolution
+const char* ssid     = "ssid";
+const char* password = "password";
+
+#include <WebOTA.h>
+WebServer OTAServer(8080);
+
+#define LED_PIN 2
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+	Serial.begin(115200);
+
+	// initialize digital pin LED_PIN as an output.
+	pinMode(LED_PIN, OUTPUT);
+
+	init_wifi(ssid, password, host);
+	init_web_ota(&OTAServer);
+}
+
+// the loop function runs over and over again forever
+void loop() {
+	int md = 1000;
+
+	digitalWrite(LED_PIN, HIGH);
+	delay(md);
+	digitalWrite(LED_PIN, LOW);
+	delay(md);
+
+	OTAServer.handleClient();
+}
